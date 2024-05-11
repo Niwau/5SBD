1bf82ff9df3bd95e834308430f3a839f2ca677cf
Guilherme <xxguilhermesoaresxx@gmail.com>
2024-05-11 17:14:04 -0300

    note: need to update stock

--- av1/script.sql
+++ av1/script.sql
@@ -1,9 +1,109 @@
+-- pedidos
+CREATE TYPE OrderStatus AS ENUM ('PAGAMENTO PENDENTE', 'PAGAMENTO REALIZADO', 'FINALIZADO');
+
+CREATE TABLE IF NOT EXISTS Orders (
+    id SERIAL PRIMARY KEY,
+	order_id VARCHAR(255) UNIQUE,
+    purchase_date DATE,
+    payments_date DATE,
+    buyer_email VARCHAR(255),
+    buyer_name VARCHAR(255),
+    cpf VARCHAR(14),
+    buyer_phone_number VARCHAR(20),
+    ship_address_1 VARCHAR(255),
+    ship_address_2 VARCHAR(255),
+    ship_address_3 VARCHAR(255),
+    ship_city VARCHAR(100),
+    ship_state VARCHAR(100),
+    ship_postal_code VARCHAR(20),
+    ship_country VARCHAR(100),
+    status OrderStatus
+);
+
+-- itens do pedido
+CREATE TABLE OrderItems (
+    id SERIAL PRIMARY KEY,
+    order_id VARCHAR(255),
+    sku VARCHAR(255),
+    product_name VARCHAR(255),
+    purchased_quantity INT,
+    currency VARCHAR(3),
+    item_price DECIMAL(10, 2),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id)
+);
+
+-- clientes
+CREATE TABLE Customers (
+    id SERIAL PRIMARY KEY,
+    cpf VARCHAR(14) UNIQUE,
+    name VARCHAR(255),
+    email VARCHAR(255),
+    phone VARCHAR(20)
+);
+
+-- produtos
+CREATE TABLE Products (
+    id SERIAL PRIMARY KEY,
+    sku VARCHAR(255) UNIQUE,
+    name VARCHAR(255),
+    available_quantity INT DEFAULT 0
+);
+
+-- movimentação de estoque
+CREATE TYPE MovementType AS ENUM ('ENTRADA', 'SAÍDA');
+
+CREATE TABLE StockMovements (
+    id SERIAL PRIMARY KEY,
+    product_id INT,
+    quantity INT,
+    price DECIMAL(10, 2),
+    movement_type MovementType,
+    movement_date DATE,
+    FOREIGN KEY (product_id) REFERENCES Products(id)
+);
+
+
+-- compras
+CREATE TABLE Restocking (
+    id SERIAL PRIMARY KEY,
+    product_id INT,
+    needed_quantity INT,
+    FOREIGN KEY (product_id) REFERENCES Products(id)
+);
+
+-- CRIANDO TABELA DE CARGA
+CREATE TEMP TABLE IF NOT EXISTS OrderLoader (
+    order_id VARCHAR(255),
+    order_item_id INT,
+    purchase_date DATE,
+    payments_date DATE,
+    buyer_email VARCHAR(255),
+    buyer_name VARCHAR(255),
+    cpf VARCHAR(14),
+    buyer_phone_number VARCHAR(20),
+    sku VARCHAR(255),
+    product_name VARCHAR(255),
+    purchased_quantity INT,
+    currency VARCHAR(10),
+    item_price DECIMAL(10, 2),
+    ship_service_level VARCHAR(255),
+    recipient_name VARCHAR(255),
+    ship_address_1 VARCHAR(255),
+    ship_address_2 VARCHAR(255),
+    ship_address_3 VARCHAR(255),
+    ship_city VARCHAR(100),
+    ship_state VARCHAR(100),
+    ship_postal_code VARCHAR(20),
+    ship_country VARCHAR(100),
+    ioss_number VARCHAR(255)
+);
+
 -- CARREGA O CSV DE PEDIDOS PARA A TABELA TEMPORÁRIA
-COPY OrderLoader FROM './pedido.sql' DELIMITER ',' CSV HEADER;
+COPY OrderLoader FROM 'C:\Users\Guilherme\Desktop\pedido.csv' DELIMITER ',' CSV HEADER;
 
 -- INSERINDO OS CLIENTES QUE NÃO ESTÃO CADASTRADOS
-INSERT INTO Customers 
-    SELECT cpf, buyer_name, buyer_email, buyer_phone_number
+INSERT INTO Customers (cpf, name, email, phone)
+    SELECT DISTINCT cpf, buyer_name, buyer_email, buyer_phone_number
     FROM OrderLoader WHERE NOT EXISTS (
         SELECT 1
         FROM Customers
@@ -11,9 +111,25 @@ INSERT INTO Customers
     );
 
 -- INSERINDO PEDIDOS E ALTERANDO STATUS
-INSERT INTO Orders
-    SELECT
-        order_id,
+INSERT INTO Orders (
+		order_id,
+        purchase_date,
+        payments_date,
+        buyer_email,
+        buyer_name,
+        cpf,
+        buyer_phone_number,
+        ship_address_1,
+        ship_address_2,
+        ship_address_3,
+        ship_city,
+        ship_state,
+        ship_postal_code,
+        ship_country,
+		status
+	)
+    SELECT DISTINCT
+		order_id,
         purchase_date,
         payments_date,
         buyer_email,
@@ -28,8 +144,8 @@ INSERT INTO Orders
         ship_postal_code,
         ship_country,
         CASE
-            WHEN payment_date IS NOT NULL THEN 'PAGAMENTO REALIZADO'
-            ELSE 'PAGAMENTO PENDENTE'
+            WHEN payments_date IS NOT NULL THEN 'PAGAMENTO REALIZADO'::OrderStatus
+            ELSE 'PAGAMENTO PENDENTE'::OrderStatus
         END AS status
     FROM OrderLoader;
 
@@ -40,7 +156,7 @@ INSERT INTO OrderItems (order_id, sku, product_name, purchased_quantity, currenc
 
 -- INSERINDO PRODUTOS QUE NÃO ESTÃO CADASTRADOS
 INSERT INTO Products (sku, name)
-    SELECT sku, product_name FROM OrderLoader
+    SELECT DISTINCT sku, product_name FROM OrderLoader
     WHERE NOT EXISTS ( 
         SELECT 1
         FROM Products
@@ -56,33 +172,11 @@ INSERT INTO StockMovements (product_id, quantity, price, movement_type, movement
         'SAÍDA',
         Orders.purchase_date
     FROM OrderItems
-    JOIN Orders ON Orders.order_id = OrderItems.id
-    ORDER BY OrderItems.purchased_quantity * OrderItems.item_price;
+    JOIN Orders ON Orders.order_id = OrderItems.order_id
+    ORDER BY OrderItems.purchased_quantity * OrderItems.item_price DESC;
 
 -- VERIFICAR E ATUALIZAR O ESTOQUE
-WITH MovementItems AS (
-    SELECT
-        product_id,
-        SUM(CASE WHEN movement_type = 'SAÍDA' THEN -quantity ELSE quantity END) AS total_quantity
-    FROM StockMovements
-    GROUP BY product_id
-);
 
--- ATUALIZAR A QUANTIDADE DISPONÍVEL DE PRODUTOS
-UPDATE Products p
-SET available_quantity = available_quantity - im.total_quantity
-FROM MovementItems im
-WHERE p.id = im.product_id
-AND p.available_quantity >= im.total_quantity;
-
--- ADICIONAR REGISTROS DE REABASTECIMENTO DE ESTOQUE
-INSERT INTO Restocking (product_id, needed_quantity)
-SELECT
-    im.product_id,
-    -im.total_quantity  -- Quantidade negativa indica que é necessário reabastecer o estoque
-FROM MovementItems im
-JOIN Products p ON p.id = im.product_id
-WHERE p.available_quantity < im.total_quantity;
 
 -- LIMPANDO A TABELA TEMPORÁRIA
 TRUNCATE TABLE OrderLoader;
